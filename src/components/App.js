bc8102aad05019da62ae9c621fd90276088df70f
Lili Boxer <ninaliboxer@gmail.com>
2019-08-19 13:50:56 -0700

    text selector rendering through app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import TextSelector from './TextSelector';
 
 export default function App() {
-  return <h1>React Quiz</h1>;
+  return (
+    <>
+      <h1>React Quiz</h1>
+      <TextSelector />
+    </>
+  );
 }
   
--- src/components/TextSelector.js
+++ src/components/TextSelector.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-function TextSelector() {
-
+function TextSelector({ text }) {
+  return (
+    <form>
+      <input type="text" value={text} placeholder="what's yo name?"></input>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 TextSelector.propTypes = {
-
+  text: PropTypes.string.isRequired
 };
 
 export default TextSelector;
