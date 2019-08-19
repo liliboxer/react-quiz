44f886195fe0b1de6ef93a2273129bada826894d
Lili Boxer <ninaliboxer@gmail.com>
2019-08-19 13:52:16 -0700

    shape component written out

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Shape() {
-
+function Shape({ text }) {
+  return (
+    <>
+      <p>{text}</p>
+    </>
+  );
 }
 
 Shape.propTypes = {
-
+  text: PropTypes.string.isRequired
 };
 
 export default Shape;
