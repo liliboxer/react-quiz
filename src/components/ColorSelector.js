2e97fd189f4a264eb77c30745b728d74d867fde9
Lili Boxer <ninaliboxer@gmail.com>
2019-08-19 14:24:03 -0700

    color selector form built out

--- src/components/ColorSelector.js
+++ src/components/ColorSelector.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ColorSelector() {
-
+function ColorSelector({ fontColor, backgroundColor }) {
+  return (
+    <form>
+      <input type="color" name="fontColor" value={fontColor} onChange={this.handleFontColorChange}></input>
+      <input type="color" name="backgroundColor" value={backgroundColor} onChange={this.handleBackgroundColorChange}></input>
+    </form>
+  );
 }
 
 ColorSelector.propTypes = {
-
+  fontColor: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired
 };
 
 export default ColorSelector;
